10531897970acfd592b5075f67ded5d5aab28198
Temi <temiapantaku@gmail.com>
2022-06-20 18:03:00 +0100

    print details to screen using js

---
+++ script.js
@@ -0,0 +1,9 @@
+var myName = document.getElementById("name");
+var myHeight = document.getElementById("height");
+var myCountry = document.getElementById("country");
+
+function printFunction() {
+    myName.innerHTML = "My name is Temi Apantaku";
+    myHeight.innerHTML = "I am 160cm tall";
+    myCountry.innerHTML = "I am from Nigeria";
+}
